259d3ab2c1a0c54bf243c1cb66b881747c9a8926
Brandon Leeds <brandonleeds@gmail.com>
2017-12-07 22:17:09 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -18,5 +18,5 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  text.match(/[1-9]/) != nil ? true : false
+  phone.match(/[1-9]/) != nil ? true : false
 end
